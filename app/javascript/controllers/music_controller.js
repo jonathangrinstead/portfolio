25f8d2b0f772de4e947be75d17d56b85f1b73a76
Jonathan Grinstead <jonathangrinstead.design@gmail.com>
2023-12-29 15:06:27 +0000

    Music section now working

--- app/javascript/controllers/music_controller.js
+++ app/javascript/controllers/music_controller.js
@@ -2,12 +2,19 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="music"
 export default class extends Controller {
-  static values = {
-    client: String,
-    clientSecret: String
-  }
+  static values = { authToken: String }
+  static targets = ["img", "name", "song"]
   connect() {
-    console.log(this.clientValue)
-    console.log(this.clientSecretValue)
+    const headers = {'Authorization': `Bearer ${this.authTokenValue}`};
+    fetch('https://api.spotify.com/v1/me/player/recently-played', {headers})
+      .then(response => response.json ())
+      .then ((data) => {
+        const img = data.items[0].track.album.images[1].url
+        const name = data.items[0].track.album.artists[0].name
+        const song = data.items[0].track.name
+        this.imgTarget.innerHTML = `<img src="${img}" alt="artwork_img" style="width:150px; height:150px; border-radius:20px;">`
+        this.nameTarget.innerText = name
+        this.songTarget.innerText = song
+    });
   }
 }
