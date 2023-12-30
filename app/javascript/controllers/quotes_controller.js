8473cab3d03592c30e2238c175d205c55e296132
Jonathan Grinstead <jonathangrinstead.design@gmail.com>
2023-12-30 16:00:43 +0000

    refresh button working + small bug fixes

--- app/javascript/controllers/quotes_controller.js
+++ app/javascript/controllers/quotes_controller.js
@@ -5,16 +5,27 @@ export default class extends Controller {
     static targets = ["content", "author"]
   connect() {
   }
-  refresh() {
+  refresh(event) {
+    const button = event.currentTarget;
+    button.classList.add('fa-spin');
     fetch('https://raw.githubusercontent.com/skolakoda/programming-quotes-api/master/Data/quotes.json')
-    .then(response => response.json ())
-    .then ((data) => {
-      console.log(data)
-      const randQuote = data[Math.floor(Math.random()*data.length)];
-      const quoteTag = `❝ ${randQuote.en} ❞`
-      const authorTag = `- ${randQuote.author}`
-      this.contentTarget.innerText = quoteTag
-      this.authorTarget.innerText = authorTag
-    });
+      .then(response => response.json())
+      .then(data => {
+        if (data && data.length > 0) {
+          const randQuote = data[Math.floor(Math.random() * data.length)];
+          const quoteTag = `❝ ${randQuote.en} ❞`;
+          const authorTag = `- ${randQuote.author}`;
+          this.contentTarget.innerText = quoteTag;
+          this.authorTarget.innerText = authorTag;
+        } else {
+          console.error('Empty or invalid data array');
+        }
+      })
+      .catch(error => console.error('Error fetching data:', error))
+      .finally(() => {
+        setTimeout(() => {
+          button.classList.remove('fa-spin');
+        }, 1000);
+      });
   }
 }
