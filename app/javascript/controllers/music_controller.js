3b4530a259d60981c6c3f74ca45db55de7be06d1
Jonathan Grinstead <jonathangrinstead.design@gmail.com>
2023-12-30 13:47:46 +0000

    added link to artist from music tab

--- app/javascript/controllers/music_controller.js
+++ app/javascript/controllers/music_controller.js
@@ -14,9 +14,10 @@ export default class extends Controller {
         const img = data.items[0].track.album.images[1].url;
         const name = data.items[0].track.album.artists[0].name;
         const song = data.items[0].track.name;
+        const url = data.items[0].context.external_urls.spotify;
 
         this.imgTarget.innerHTML = `<img src="${img}" alt="artwork_img" style="width:150px; height:150px; border-radius:20px; box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;">`;
-        this.nameTarget.innerText = name;
+        this.nameTarget.innerHTML = `<a href="${url}" target="_blank">${name}</a>`;
         this.songTarget.innerText = song;
       })
       .catch(error => {
